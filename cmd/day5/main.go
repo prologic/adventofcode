3ce7e260cdc2fc1fe2659bc9b64666cab965bbff
James Mills <prologic@shortcircuit.net.au>
2020-12-06 10:58:29 +1000

    Day 5 Part 1

--- cmd/day5/main.go
+++ cmd/day5/main.go
@@ -1,20 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/prologic/aoc"
 )
 
-func main() {
-	s := os.Args[1]
+func readBoardingpassesFromReader(r io.Reader) (bps []aoc.Boardingpass, err error) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		bp, err := aoc.ParseBoardingpass(line)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error parsing boarding pass %s: %s", line, err)
+			continue
+		}
 
-	bp, err := aoc.ParseBoardingpass(s)
+		bps = append(bps, bp)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading standard input: %w", err)
+	}
+
+	return
+}
+
+func main() {
+	bps, err := readBoardingpassesFromReader(os.Stdin)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error parsing boarding pass: %s", err)
+		fmt.Fprintf(os.Stderr, "error reading input: %s", err)
 		os.Exit(2)
 	}
 
-	fmt.Printf("%s\n", bp)
+	fmt.Fprintf(os.Stderr, "%d boarding passes found\n", len(bps))
+
+	var highestSeatID int
+
+	for _, bp := range bps {
+		fmt.Fprintf(os.Stderr, "%s\n", bp)
+		if bp.SeatID() > highestSeatID {
+			highestSeatID = bp.SeatID()
+		}
+	}
+
+	fmt.Printf("%d\n", highestSeatID)
 }
